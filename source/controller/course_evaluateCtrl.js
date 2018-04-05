084f6b9e5130c2981807620b238755f3f2e6c169
陈阳阳 <chen_yangyang@venustech.com.cn>
2018-04-05 21:08:31 +0800

    课程评价

--- source/controller/course_evaluateCtrl.js
+++ source/controller/course_evaluateCtrl.js
@@ -5,4 +5,23 @@ app.controller('course_evaluateCtrl', ['$scope',function($scope){
 
     $scope.data='课程评价页面';
 
+    $scope.totalItems  = 64;
+	$scope.currentPage = 3;
+	$scope.itemsPerPage = 3;
+	$scope.maxSize = 5;
+
+	$scope.setPage = function (pageNo) {
+	  $scope.currentPage = pageNo;
+	}
+	$scope.pageChanged = function() {
+	  $log.log('Page changed to: ' + $scope.currentPage);
+	}
+
+	$scope.imgUrl = 'images/evaluate/avater.png';
+	
+	// 点击头像，高亮
+	$scope.clickImg = function(){
+		$scope.imgUrl = 'images/evaluate/avater-active.png';
+	};
+
 }]);
