68aca441bd54f43ebdbb529ba8873b70780d6935
Ailian0206 <ailian@163.com>
2018-05-02 23:18:19 +0800

    自适应

--- source/controller/digitization_courseCtrl.js
+++ source/controller/digitization_courseCtrl.js
@@ -12,10 +12,10 @@ app.controller('digitization_courseCtrl', ['$scope',function($scope){
                 $scope.mainList= $scope.mainList1;
                 break;
             case 1:
-                $scope.mainList=$scope.mainList1.slice(0,5);
+                $scope.mainList=$scope.mainList1.slice(0,4);
                 break;
             case 2:
-                $scope.mainList=$scope.mainList1.slice(5,10);
+                $scope.mainList=$scope.mainList1.slice(4,7);
 
         }
     };
@@ -28,10 +28,8 @@ app.controller('digitization_courseCtrl', ['$scope',function($scope){
         {img:'images/digitization_course/img1.png',name:'测量仓鼠体重5'},
         {img:'images/digitization_course/img1.png',name:'测量仓鼠体重6'},
         {img:'images/digitization_course/img1.png',name:'测量仓鼠体重7'},
-        {img:'images/digitization_course/img1.png',name:'测量仓鼠体重8'},
-        {img:'images/digitization_course/img1.png',name:'测量仓鼠体重9'},
-        {img:'images/digitization_course/img1.png',name:'测量仓鼠体重10'}  ];
 
+    ]
 
 
     $scope.mainList=$scope.mainList1;
