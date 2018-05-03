a0779c96313a4509f2adc07b2991e17e17424689
Ailian0206 <ailian@163.com>
2018-05-03 23:48:17 +0800

    webapp

--- source/controller/wicro_course.js
+++ source/controller/wicro_course.js
@@ -14,13 +14,13 @@ app.controller('wicro_courseCtrl', ['$scope',function($scope){
                 $scope.mainList= $scope.mainList1;
                 break;
             case 1:
-                $scope.mainList=$scope.mainList1.slice(0,4);
+                $scope.mainList=$scope.mainList1.slice(0,3);
                 break;
             case 2:
-                $scope.mainList=$scope.mainList1.slice(3,8);
+                $scope.mainList=$scope.mainList1.slice(3,5);
                 break;
             case 3:
-                $scope.mainList=$scope.mainList1.slice(8,10);
+                $scope.mainList=$scope.mainList1.slice(5,7);
 
         }
     };
@@ -33,9 +33,7 @@ app.controller('wicro_courseCtrl', ['$scope',function($scope){
         {img:'images/digitization_course/img1.png',name:'测量仓鼠记忆力问题5'},
         {img:'images/digitization_course/img1.png',name:'测量仓鼠记忆力问题6'},
         {img:'images/digitization_course/img1.png',name:'测量仓鼠记忆力问题7'},
-        {img:'images/digitization_course/img1.png',name:'测量仓鼠记忆力问题8'},
-        {img:'images/digitization_course/img1.png',name:'测量仓鼠记忆力问题9'},
-        {img:'images/digitization_course/img1.png',name:'测量仓鼠记忆力问题10'}  ];
+        ];
 
 
 
